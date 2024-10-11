7f416e5dcb671c4efbcd27927b95e866fb0bc886
mohamedHashem250 <106890469+mohamedHashem250@users.noreply.github.com>
2024-10-11 12:14:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import tensorflow as tf
 from PIL import Image
+from flower_classifier import flower_classification
 
 # Your classifier code here
 def classify_image(image):
@@ -9,10 +10,22 @@ def classify_image(image):
 
 st.title("Deep Learning Classifier")
 uploaded_file = st.file_uploader("Choose an image", type="jpg")
-
+st.header("Flower Classification Example")
+st.text("Upload a Flower Image for flower classification as SunRose , Rose, Diasy")
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image', use_column_width=True)
     st.write("Classifying...")
     label = classify_image(image)
     st.write(f"Prediction: {label}")
+    label = teachable_machine_classification(image, 'hyper_tuned_model.h5')
+    if label == 'daisy':
+  	    st.write("The Flower is  daisy")
+    elif label ==  'dandelion':
+  	    st.write("The Flower is  daisy")
+    elif label ==  'rose':
+  	    st.write("The Flower is  rose")
+    elif label == 'sunflower':
+  	    st.write("The Flower is  Sunflower")
+    else:
+  	    st.write("The Flower is  tulip")
